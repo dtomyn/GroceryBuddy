66b7cd34a7ed3f54f0491b5b9ab25b4bede51861
Darek <dtomyn@hotmail.com>
2013-03-25 12:16:59 -0600

    much better version

--- MockScreens/Scripts/Custom/ShoppingCart.js
+++ MockScreens/Scripts/Custom/ShoppingCart.js
@@ -1,17 +1,41 @@
-﻿$(function () {
+﻿/// <reference path="../jquery-1.9.1.js" />
+/// <reference path="../jquery.mobile-1.3.0.js" />
+/// <reference path="../knockout-2.2.1.debug.js" />
+$(function () {
 
     // Overall view model for the application
 
     // Class to represent a grocery cart
-    function GroceryCart(name, items) {
+    var GroceryCart = function (name, items) {
+//    function GroceryCart(name, items) {
         var self = this;
         self.name = ko.observable(name);
         self.numberOfItems = items;
+
+        self.addCartItem = function (name, category, numberOfPieces, size, measurement) {
+
+        };
         //TODO: reminder
         //TODO: count cart items
-    }
 
-    function ShoppingCartViewModel() {
+        return self;
+    };
+//    }
+
+    var CartItem = function (parentCart, name, category, numberOfPieces, size, measurement) {
+//    function CartItem(parentCart, name, category, numberOfPieces, size, measurement) {
+        var self = this;
+        self.parentCart = parentCart;
+        self.name = ko.observable(name);
+        self.category = ko.observable(category);
+        self.numberOfPieces = ko.observable(numberOfPieces);
+        self.size = ko.observable(size);
+        self.measurement = ko.observable(measurement);
+    };
+//    }
+
+    var shoppingCartViewModel = (function () {
+//    function ShoppingCartViewModel() {
         var self = this;
 
         // Non-editable category data - would come from the server
@@ -21,12 +45,16 @@
             { categoryName: "Ultimate (whole zebra)", icon: "TODO" }
         ];
 
+        //getCategories();
+
         // Editable data
         self.carts = ko.observableArray([
             new GroceryCart("Shopping Cart 1", 25),
             new GroceryCart("Shopping Cart 2", 4)
         ]);
-    }
+    })();
+//    }
 
-    ko.applyBindings(new ShoppingCartViewModel());
+    ko.applyBindings(shoppingCartViewModel);
+//    ko.applyBindings(new ShoppingCartViewModel());
 });
